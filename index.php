0a6ed4d35e3dbdcf7f018ff341f37cb7fb0b14b0
SpectreFromTheFuture <spectrefromthefuture@gmail.com>
2022-04-30 10:45:55 +0400

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+<head>
+    <meta charset = "UTF-8">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel = "stylesheet" href ="css/index.css">
+</head>
+<body>
+
+</body>
+</html>
