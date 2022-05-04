d23202e31a7018f701ad47dcb2f6fa837c961589
SpectreSpect <spectrefromthefuture@gmail.com>
2022-05-04 15:26:24 +0400

    It works, but there is some visual defect

--- index.php
+++ index.php
@@ -7,5 +7,17 @@
 </head>
 <body>
 
+    <input id = "input_columnsCount" type = "text" placeholder = "Columns count">
+    <input id = "input_rowsCount" type = "text" placeholder = "Rows count"><br><br>
+    <textarea id = input_pairs></textarea><br>
+    <p id = "output"></p>
+    <button type = "button" onclick = "getDescription()">Press</button>
+
+    <p>This relation is transitive, reflective and etc</p>
+
+    <h1 id = "output_heading">Test name</h1>
+    <textarea id = "input_text" type = "text" placeholder = "Enter the key"></textarea>
+    <button type = "button" onclick = "testFunction()">Press</button>
+    <script src = "js/index.js"></script>
 </body>
 </html>
